d857e8dfdd632571f12b5c0fd9bf37fc3513e02b
Anthony Grimes <i@raynes.me>
2013-02-26 16:33:17 -0800

    Learned to read and write.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.flatland/useful "0.9.2"]
+                 [org.flatland/useful "0.9.4"]
                  [gloss "0.2.2-beta5"]])
--- src/flatland/casette.clj
+++ src/flatland/casette.clj
@@ -1,3 +1,39 @@
 (ns flatland.casette
-  (:require [gloss.core :refer [compile-frame]]
-            [gloss.io :refer [encode]]))
+  (:require [gloss.core :refer [compile-frame finite-frame]]
+            [gloss.core.protocols :refer [write-bytes]]
+            [gloss.io :refer [lazy-decode-all encode]]
+            [flatland.useful.map :refer [keyed]]
+            [flatland.useful.io :refer [mmap-file]])
+  (:import [java.io RandomAccessFile]))
+
+(defn message-codec [codec]
+  (finite-frame :int32 codec))
+
+(defn grow-file [^RandomAccessFile file size]
+  ;; Should create a sparse file on Linux and Windows, but apparently not OS X (HFS+).
+  (.setLength file size))
+
+(defn ^:private write-message!
+  ([frame buffer value]
+     (write-message! frame buffer (.position buffer) value))
+  ([frame buffer offset value]
+     (let [encoded (encode (compile-frame (message-codec frame)) value)]
+       (.position buffer offset)
+       (doseq [buf encoded]
+         (.put buffer (.array buf)))
+       buffer)))
+
+(defn read-messages
+  ([frame buffer] (read-messages frame buffer 0))
+  ([frame buffer offset]
+     (let [codec (compile-frame (message-codec frame))]
+       (.position buffer offset)
+       (take-while (complement nil?) (lazy-decode-all codec buffer)))))
+
+(defn create
+  "Create a new log file."
+  ([path] (create path 524288000))
+  ([path size]
+     (mmap-file
+      (doto (RandomAccessFile. path "rw")
+        (grow-file size)))))
