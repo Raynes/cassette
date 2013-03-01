b035abdff25f90e2c6f1ab8f3cc6f8db54cdfabd
Alan Malloy <alan@malloys.org>
2013-02-28 16:55:02 -0800

    Some WIP on reading kafka files

--- src/flatland/cassette/codec.clj
+++ src/flatland/cassette/codec.clj
@@ -1,9 +1,13 @@
 (ns flatland.cassette.codec
   (:require [gloss.core :as gloss :refer [compile-frame finite-frame]]
             [gloss.io :as io]
-            [gloss.core.codecs :as codecs])
+            [gloss.core.codecs :as codecs]
+            [flatland.useful.io :refer [mmap-file]]
+            [me.raynes.fs :as fs])
   (:import java.util.zip.CRC32
-           (java.nio ByteBuffer)))
+           (java.nio ByteBuffer)
+           (java.io StringReader)
+           (java.util Collections Scanner)))
 
 (defn len [^ByteBuffer buf]
   (- (.limit buf) (.position buf)))
@@ -39,3 +43,31 @@
                        (throw (IllegalArgumentException.
                                (format "Magic byte %d does not match: expected %d"
                                        magic magic-byte))))))))
+
+(defn kafka-file
+  ([{:keys [path]}]
+     (kafka-file {:path path} 99999999999))
+  ([{:keys [path]} byte-offset]
+     (let [files (.listFiles path)
+           expected-name (format "%011d.kafka" byte-offset)
+           acceptable-files (remove #(neg? (compare expected-name (.getName %)))
+                                    files)]
+       (Collections/max acceptable-files))))
+
+(defn first-offset [filename]
+  (let [scanner (doto (Scanner. (StringReader. filename))
+                  (.useDelimiter #"\."))]
+    (.nextLong scanner)))
+
+(defn read-one [buffer codec]
+  (let [[success x remainder] (read-bytes codec buffer)]
+    (if success
+      {:success true, :value x}
+      {:success false})))
+
+(defn read-message [{:keys [path codec]} byte-offset]
+  (let [file (kafka-file path)
+        offset (first-offset (fs/name file))
+        {:keys [^ByteBuffer buffer close]} (mmap-file file)]
+    (.position buffer (- byte-offset first-offset))
+    (read-one buffer codec)))
