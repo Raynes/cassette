fb6e6b72d58f7329e52ced20f1fb4c23263514bb
Anthony Grimes <i@raynes.me>
2013-03-01 17:50:51 -0800

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/cassette "0.1.0"
+(defproject org.flatland/cassette "0.1.1"
   :description "A Clojure implementation of the Kafka file format."
   :url "https://github.com/flatland/cassette"
   :license {:name "Eclipse Public License"
