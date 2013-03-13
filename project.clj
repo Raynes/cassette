6967790b2dc1636da9efc224f75ec74012d7c7b3
Alan Malloy <alan@malloys.org>
2013-03-13 14:43:48 -0700

    Version 0.1.2, with messages-since support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/cassette "0.1.1"
+(defproject org.flatland/cassette "0.1.2"
   :description "A Clojure implementation of the Kafka file format."
   :url "https://github.com/flatland/cassette"
   :license {:name "Eclipse Public License"
