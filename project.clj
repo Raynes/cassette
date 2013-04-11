c434212f73b75bae9c8b085e2a126ad0bc08ad87
Alan Malloy <alan@malloys.org>
2013-04-11 16:44:49 -0700

    Don't use to-fix; its &rest args were slowing us down.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/cassette "0.2.2"
+(defproject org.flatland/cassette "0.2.3"
   :description "A Clojure implementation of the Kafka file format."
   :url "https://github.com/flatland/cassette"
   :license {:name "Eclipse Public License"
